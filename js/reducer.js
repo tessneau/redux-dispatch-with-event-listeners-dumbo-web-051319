0d76bc1fee1b4ce3d055780d061c733849c9024b
Tess Neau <tessneau@gmail.com>
2019-08-07 16:01:07 -0400

    done

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,28 @@
 // add code snippets from README
+let state;
+let button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+  dispatch({type: 'INCREASE_COUNT'})
+})
+
+function dispatch(action){
+  state = reducer(state, action);
+  render()
+}
+
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+dispatch({type: '@@INIT'})
